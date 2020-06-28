4999530e43ed2770d4f1978a6b6a8934f31309a5
ruslan-ok <ok@rusel.by>
2020-06-28 16:03:22 +0300

    ToDo - вложения, заметки, список
    Реализованы следующие возможности в приложении ToDo:
    1) добавление заметок для задачи
    2) добавление файлов в задаче
    3) перемещение задачи в другой список

--- rusel/urls.py
+++ rusel/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 from . import utils
 
@@ -46,4 +49,4 @@ urlpatterns = i18n_patterns(
 
     path('account/', include('account.urls')),
     path('admin/',   admin.site.urls, name='admin'),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
