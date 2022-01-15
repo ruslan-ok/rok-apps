5080eaa996856f2a4d3ed17f599027e82322f684
Ruslan Akunevich <ok@rusel.by>
2022-01-15 16:09:20 +0300

    Actual software versions.

--- rusel/views.py
+++ rusel/views.py
@@ -1,4 +1,4 @@
-import datetime, django, OpenSSL, ssl
+import datetime, django, OpenSSL, ssl, urllib.request
 from platform import python_version
 from django.http import HttpResponse
 from django.template import loader
@@ -8,6 +8,7 @@ from django.conf import settings
 from rusel.context import get_base_context
 #from trip.models import trip_summary
 #from rusel.site_stat import get_site_stat
+from .mysql_ver import get_mysql_ver
 
 from task.const import APP_HOME, ROLE_ACCOUNT
 from task.models import Task
@@ -57,13 +58,17 @@ class ListView(BaseListView, TuneData):
             #context['trip_summary'] = trip_summary(request.user.id)
             context['python_version'] = python_version()
             context['django_version'] = '{}.{}.{} {}'.format(*django.VERSION)
-            context['apache_version'] = '2.4.41 (Win64)'
+            response = urllib.request.urlopen('https://rusel.by')
+            versions = response.headers['Server'].split(' ')
+            for ver in versions:
+                if 'Apache' in ver:
+                    context['apache_version'] = ver.split('/')[1]
+                if 'OpenSSL' in ver:
+                    context['openssl_version'] = ver.split('/')[1]
+                if 'mod_wsgi' in ver:
+                    context['mod_wsgi_version'] = ver.split('/')[1]
             context['hmail_version'] = '5.6.7 - Build 2425'
-        
-            #cursor = connection.cursor()
-            #cursor.execute('SHOW VARIABLES LIKE "version"')
-            #context['mysql_version'] = cursor.fetchone()
-            context['mysql_version'] = '8.0.19'
+            context['mysql_version'] = get_mysql_ver()
         
             try:
                 cert = ssl.get_server_certificate(('rusel.by', 443))
