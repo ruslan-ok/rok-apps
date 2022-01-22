8edd55b451b12b678b78c778afac5d68131da7c8
Ruslan Akunevich <ok@rusel.by>
2022-01-22 16:14:32 +0300

    iziToast connected.

--- static/js/store.js
+++ static/js/store.js
@@ -130,17 +130,10 @@ function copyToClipboard(fld, kind) {
         return;
       }
       navigator.clipboard.writeText(value).then(function() {
-        alert(kind, 'Copied to clipboard.', 'success');
+        iziToast.success({title: 'Succsess', message: 'Copied to clipboard.', position: 'bottomRight'});
       }, function(err) {
-        alert(kind, 'Could not copy text: ' + err, 'danger');
+        iziToast.error({title: 'Error', message: 'Could not copy text: ' + err, position: 'bottomRight'});
       });
-}
-
-function alert(kind, message, type) {
-    const alertPlaceholder = document.getElementById(kind + 'AlertPlaceholder');
-    const wrapper = document.createElement('div');
-    wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
-    alertPlaceholder.append(wrapper);
 }
 
 function toggleHistory(btn) {
