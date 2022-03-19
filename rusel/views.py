fb8070707dd3a2fe409887c0e4d8767ef4a39dd9
Ruslan Akunevich <ok@rusel.by>
2022-03-19 07:39:11 +0300

    Tollkits and plugins versions.

--- rusel/views.py
+++ rusel/views.py
@@ -67,9 +67,15 @@ class ListView(BaseListView, TuneData):
                     context['openssl_version'] = ver.split('/')[1]
                 if 'mod_wsgi' in ver:
                     context['mod_wsgi_version'] = ver.split('/')[1]
-            context['hmail_version'] = '5.6.7 - Build 2425'
+            context['hmail_version'] = '5.6.7 bld 2425'
             context['mysql_version'] = get_mysql_ver()
-        
+            context['leaflet_version'] = get_leaflet_ver()
+            context['bootstrap_version'] = get_bootstrap_ver()
+            context['izitoast_version'] = get_izitoast_ver()
+            context['chartjs_version'] = get_chartjs_ver()
+            context['swiper_version'] = get_swiper_ver()
+            context['firebase_version'] = get_firebase_ver()
+
             try:
                 cert = ssl.get_server_certificate(('rusel.by', 443))
                 x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
@@ -83,3 +89,23 @@ class ListView(BaseListView, TuneData):
 
         template = loader.get_template('index_user.html')
         return HttpResponse(template.render(context, request))
+
+
+def get_leaflet_ver():
+    return '1.7.1'
+
+def get_bootstrap_ver():
+    return '5.1.0'
+
+def get_izitoast_ver():
+    return '1.4.0'
+
+def get_chartjs_ver():
+    return '3.7.0'
+
+def get_swiper_ver():
+    return '8.0.7'
+
+def get_firebase_ver():
+    return '5.2.0'
+
