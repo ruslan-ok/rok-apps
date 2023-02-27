5244f779c3b26e55b91484cf256e6f21fb945bff
Ruslan Akunevich <ok@rusel.by>
2023-02-27 23:39:44 +0300

    Fix js

--- rusel/views.py
+++ rusel/views.py
@@ -37,23 +37,24 @@ class ListView(BaseListView):
         for hpw in HP_WIDGETS:
             id = hpw[0]
             css = hpw[1]
-            perm = hpw[2]
+            js = hpw[2]
+            perm = hpw[3]
             if perm and not self.request.user.has_perm(perm):
                 continue
             if perm and self.request.user.is_superuser:
                 continue
-            hp_widgets.append({'id': id, 'css': css, })        
+            hp_widgets.append({'id': id, 'css': css, 'js': js, })
         context['hp_widgets'] = hp_widgets
         return context
     
 HP_WIDGETS = [
-    ('weather', 'weather', ''),
-    ('currency', 'currency', ''),
-    ('crypto', 'crypto', 'task.view_entry'),
-    ('health', '', 'task.view_health'),
-    ('logs', 'logs', 'task.view_logs'),
-    ('todo', 'todo', 'task.view_todo'),
-    ('visited', 'visited', ''),
+    ('weather', 'weather', '', ''),
+    ('currency', 'currency', '', ''),
+    ('crypto', 'crypto', '', 'task.view_entry'),
+    ('health', '', '', 'task.view_health'),
+    ('logs', 'logs', '', 'task.view_logs'),
+    ('todo', 'todo', 'todo', 'task.view_todo'),
+    ('visited', 'visited', '', ''),
 ]
 
 def get_doc(request, role, pk, fname):
