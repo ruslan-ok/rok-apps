3ab22d658f61aea536ad99012d8f0a1d3abd2618
Ruslan Akunevich <ok@rusel.by>
2023-09-23 18:01:31 +0200

    Last operation in service intervals (todo/15289)

--- back/fuel/views/part.py
+++ back/fuel/views/part.py
@@ -1,15 +1,15 @@
 from datetime import datetime
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from task.const import NUM_ROLE_PART, ROLE_PART, ROLE_APP
+from task import const
 from task.models import Task
 from core.views import BaseListView, BaseDetailView
 from fuel.forms.part import CreateForm, EditForm
 from fuel.config import app_config
 from fuel.utils import month_declination
 
-role = ROLE_PART
-app = ROLE_APP[role]
+role = const.ROLE_PART
+app = const.ROLE_APP[role]
 
 class ListView(LoginRequiredMixin, PermissionRequiredMixin, BaseListView):
     model = Task
@@ -21,6 +21,22 @@ class ListView(LoginRequiredMixin, PermissionRequiredMixin, BaseListView):
 
     def get_template_names(self):
         return ['fuel/list.html']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        nav_role = Task.get_active_nav_item(self.request.user.id, app)
+        if nav_role:
+            if Task.objects.filter(user=self.request.user.id, app_fuel__gt=0, task_1=nav_role.id).exclude(car_odometr=None).exclude(car_odometr=0).exists():
+                last_odo = Task.objects.filter(user=self.request.user.id, app_fuel__gt=0, task_1=nav_role.id).exclude(car_odometr=None).exclude(car_odometr=0).order_by('-event')[0]
+                href = last_odo.get_absolute_url()
+                match last_odo.app_fuel:
+                    case const.NUM_ROLE_FUEL: operation = 'fueling'
+                    case const.NUM_ROLE_SERVICE: operation = 'service'
+                    case _: operation = '???'
+
+                value = 'Last operation: ' + operation + ' on ' + last_odo.event.strftime('%d.%m.%Y') + ', odometer ' + str(last_odo.car_odometr)
+                context.update({'summary': [{'color': 'black', 'value': value, 'href': href}]})
+        return context
 
 
 class DetailView(LoginRequiredMixin, PermissionRequiredMixin, BaseDetailView):
@@ -46,5 +62,5 @@ def get_info(item):
     item.actualize_role_info(app, role, attr)
 
 def add_part(user, car, name):
-    task = Task.objects.create(user=user, app_fuel=NUM_ROLE_PART, name=name, event=datetime.now(), task_1=car, part_chg_km=10000, part_chg_mo=12)
+    task = Task.objects.create(user=user, app_fuel=const.NUM_ROLE_PART, name=name, event=datetime.now(), task_1=car, part_chg_km=10000, part_chg_mo=12)
     return task
