86f809768f0fa6a47e1f88da77a3d4afb591f315
Ruslan Akunevich <ok@rusel.by>
2024-06-26 23:03:32 +0200

    CodeQL alerts

--- back/service/win-acme/copycerts.py
+++ back/service/win-acme/copycerts.py
@@ -15,7 +15,7 @@ def copycert():
     cert_dir = settings.WIN_ACME_CERT
     arh_dir = work_dir + str(datetime.date.today())
 
-    logger.info(f'Started: {work_dir=}, {arh_dir=}, {cert_dir=}')
+    logger.info('Started')
 
     shutil.rmtree(arh_dir, ignore_errors=True)
     os.mkdir(arh_dir)
@@ -36,7 +36,7 @@ def copycert():
         lines = f1.readlines()
         with open(work_dir + 'rusel.by-crt.pem', 'a') as f2:
             f2.writelines(lines)
-    logger.info(f'{cert_dir}rusel.by-chain-only.pem merget into {work_dir}rusel.by-crt.pem')
+    logger.info('cert_dir->rusel.by-chain-only.pem merget into work_dir->rusel.by-crt.pem')
 
     logger.info('Finished')
 
