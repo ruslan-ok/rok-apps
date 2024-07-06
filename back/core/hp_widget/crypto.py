34c1c0b30fd42c3c9a5ca5248dd2423426a2497a
Ruslan Akunevich <ok@rusel.by>
2024-07-06 09:16:43 +0200

    Unexpected zeros in the crypto data

--- back/core/hp_widget/crypto.py
+++ back/core/hp_widget/crypto.py
@@ -13,6 +13,7 @@ def get_crypto_data(period: ChartPeriod):
         headers = {'x-access-token': api_key, 'User-Agent': 'Mozilla/5.0'}
         # timePeriod: 1h 3h 12h 24h 7d 30d 3m 1y 3y 5y
         resp = requests.get(api_url + 'history?timePeriod=' + period.value, headers=headers)
+        prev = 0
         if resp.status_code == 200:
             ret = json.loads(resp.content)
             if ret['status'] == 'success':
@@ -22,8 +23,9 @@ def get_crypto_data(period: ChartPeriod):
                 src_data = []
                 for i in reversed(range(len(ret['data']['history']))):
                     h = ret['data']['history'][i]
-                    sd = SourceData(event=datetime.utcfromtimestamp(h['timestamp']), value=Decimal(h['price'] if h['price'] else 0))
+                    sd = SourceData(event=datetime.utcfromtimestamp(h['timestamp']), value=Decimal(h['price'] if h['price'] else prev))
                     src_data.append(sd)
+                    prev = sd.value
                 chart_points = approximate(src_data, 200)
 
     chart_config = build_chart_config('BTC/USD', chart_points, '111, 184, 71')
