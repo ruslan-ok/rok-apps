e5d2b17ffb9eae8064d9f7d92ccc7524060d6d9c
ruslan-ok <ruslan.ok@gmail.com>
2025-11-02 21:38:51 +0100

    Setting up for running in a Docker container

--- back/rok/secrets.py
+++ back/rok/secrets.py
@@ -1,4 +1,5 @@
 import json
+import os
 from pathlib import Path
 from django.core.exceptions import ImproperlyConfigured
 
@@ -8,8 +9,30 @@ with open(BASE_DIR/'rok/secrets.json') as secrets_file:
     secrets = json.load(secrets_file)
 
 def get_secret(setting, secrets=secrets):
-    """Get secret setting or fail with ImproperlyConfigured"""
+    """Get secret setting or fail with ImproperlyConfigured.
+
+    For DATABASE_POSTGRES, if environment variables are set (e.g., in devcontainer),
+    they will be used to override the JSON configuration.
+    """
     try:
-        return secrets[setting]
+        value = secrets[setting]
+
+        # Override database configuration if environment variables are set
+        if setting == 'DATABASE_POSTGRES' and isinstance(value, dict):
+            # Check if we're in a containerized environment
+            db_config = value.copy()
+            if os.environ.get('PGHOST'):
+                db_config['HOST'] = os.environ.get('PGHOST')
+            if os.environ.get('PGPORT'):
+                db_config['PORT'] = int(os.environ.get('PGPORT'))
+            if os.environ.get('PGUSER'):
+                db_config['USER'] = os.environ.get('PGUSER')
+            if os.environ.get('PGPASSWORD'):
+                db_config['PASSWORD'] = os.environ.get('PGPASSWORD')
+            if os.environ.get('PGDATABASE'):
+                db_config['NAME'] = os.environ.get('PGDATABASE')
+            return db_config
+
+        return value
     except KeyError:
         raise ImproperlyConfigured("Set the {} setting".format(setting))
